da5373abb320b1965b7aa5d5ea11a95581bb2d89
iqbal5354 <80057549+iqbal5354@users.noreply.github.com>
2025-10-02 03:35:44 +0700

    Update __init__.py

--- perintah/__init__.py
+++ perintah/__init__.py
@@ -1,18 +1,22 @@
-# perintah/__init__.py
-
-def hello():
-    return "📂 Folder perintah siap dipakai."
 from .commands import register_commands, init_owner
 from .trx import register_trx
 from .ai import register_ai, init_owner_ai
 from .speedtest import register_speedtest, init_owner_speed
 
+
 async def init_all_owner(client):
+    """
+    Inisialisasi semua OWNER_ID untuk tiap modul.
+    """
     await init_owner(client)
     await init_owner_ai(client)
     await init_owner_speed(client)
 
+
 def init(client):
+    """
+    Daftarkan semua perintah dari folder perintah.
+    """
     register_commands(client)
     register_trx(client)
     register_ai(client)
