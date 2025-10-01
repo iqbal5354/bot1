6914018cbca8d8c6866e9ef54164c71d6bd8b247
iqbal5354 <80057549+iqbal5354@users.noreply.github.com>
2025-10-02 03:39:14 +0700

    Update trx.py

--- trx.py
+++ trx.py
@@ -1,11 +1,18 @@
 from telethon import events
 
-def init_trx(client):
+OWNER_ID = None
 
+async def init_owner(client):
+    global OWNER_ID
+    me = await client.get_me()
+    OWNER_ID = me.id
+
+
+def register_trx(client):
     @client.on(events.NewMessage(pattern=r"^\.isi$"))
-    async def isi_handler(event):
-        await event.respond(
-            "Jika sudah sepakat #DONE\n"
+    async def handler_isi(event):
+        await event.edit(
+            "Jika sudah sepakat #DONE\n\n"
             "Penjual Silahkan Di isi :\n\n"
             "klik  1x text kutipan di bawah untuk COPY\n"
             "```\n"
@@ -20,23 +27,24 @@ def init_trx(client):
         )
 
     @client.on(events.NewMessage(pattern=r"^\.danamasuk$"))
-    async def danamasuk_handler(event):
-        await event.respond(
+    async def handler_danamasuk(event):
+        await event.edit(
             "**:: Uang sudah masuk di saya. Silahkan kalian serah terima data ::**\n\n"
             "```\n"
             "━━━━━━━━PENTING!!━━━━━━━━\n"
             "⚠️Harap Tanyakan dulu masalah Garansi.\n"
-            "⚠️Jgn coba2 ada drama jika tidak mau saya mintain ident via VC. Karena drama=ripper.\n"
-            "⚠️Jangan Berikan Hal2 yg rawan seperti OTP tele WA OTP email di luar transaksi\n"
-            "⚠️jika Pembeli tidak ada kabar selama 8 jam maka dana akan di cairkan dan jika penjual tidak ada kabar selama 5 jam uang di transfer balik ke pembeli\n"
+            "⚠️Jgn coba2 ada drama jika tidak mau saya mintain ident via VC.\n"
+            "⚠️Jangan Berikan OTP tele/WA/email di luar transaksi.\n"
+            "⚠️Jika Pembeli tidak ada kabar 8 jam = dana cair,\n"
+            "   Jika Penjual hilang 5 jam = uang balik ke pembeli\n"
             "━━━━━━━━━━━━━━━━━━━━━━━━━━━\n"
             "```\n"
         )
 
     @client.on(events.NewMessage(pattern=r"^\.format$"))
-    async def format_handler(event):
-        await event.respond(
-            "**FORMAT TRANSAKSI\n\n**"
+    async def handler_format(event):
+        await event.edit(
+            "**FORMAT TRANSAKSI**\n\n"
             "Klik 1x text kutipan dibawah untuk COPY format\n"
             "```\n"
             "Transaksi Apa  :\n"
@@ -52,28 +60,21 @@ def init_trx(client):
         )
 
     @client.on(events.NewMessage(pattern=r"^\.aturan$"))
-    async def aturan_handler(event):
-        await event.respond(
+    async def handler_aturan(event):
+        await event.edit(
             "Selamat Datang di Rekber Warung Bullove\n\n"
             "Mohon untuk dibaca dan diperhatikan\n"
             "```——————————————————————————————————```\n"
             "**PERATURAN REKBER :**\n"
             "1. Pastikan saya admin Grub Rekber.\n"
             "2. Dilarang Kick Penjual/pembeli & Ganti Judul.\n"
-            "3. Tidak menerima REKBER barang/jasa iLEGAL (melanggar UUD)!\n"
+            "3. Tidak menerima REKBER barang/jasa iLEGAL.\n"
             "4. Mohon untuk jujur mengisi judul TRX!\n\n"
-            "**KETENTUAN TRANSAKSI :**\n"
-            "❗️Jika Pembeli tidak ada kabar selama 8 jam maka dana akan dicairkan, dan jika penjual tidak ada kabar selama 5 jam uang akan ditransfer balik ke pembeli.\n"
-            "❗️Jika Transaksi **Cancel/BATAL** Fee tetap Terpotong, jika tdk mau Terpotong Silahkan cari penjual lain.\n"
-            "❗️Transaksi akun wajib take seEMAILnya agar tidak kena HB.\n"
-            "❗️Jangan berikan hal2 yg rawan seperti OTP tele, WA, OTP email (di luar transaksi).\n"
-            "❗️INGAT SELLER MAUPUN BUYER DILARANG MENGHILANG SELAMA TRANSAKSI!\n"
-            "❗️Harap tanyakan dulu masalah Garansi.\n\n"
-            "**Nb :**\n"
-            "🔜 Admin tidak pernah merekomendasiin siapapun\n"
-            "🔜 Berani drama selama transaksi maka admin wajib meminta ident kalian berdua️\n"
-            "🔜 MENYALAHGUNAKAN TRANSAKSI UNTUK PENIPUAN/TRANSAKSI PALSU AKAN KAMI SERAHKAN KEPADA PIHAK YG BERWAJIB\n"
-            "```——————————————————————————————————```\n"
-            "Jika Setuju Silahkan Pembeli isi Format Transaksi ✔️\n\n"
+            "❗Jika Pembeli tidak ada kabar 8 jam = dana cair,\n"
+            "❗Jika Penjual hilang 5 jam = uang balik ke pembeli.\n"
+            "❗Cancel tetap potong fee.\n"
+            "❗Transaksi akun wajib take seEMAIL.\n"
+            "❗Jangan berikan OTP tele/WA/email di luar transaksi.\n"
+            "❗Seller/Buyer dilarang hilang selama transaksi.\n\n"
             "®️𝙒𝙖𝙧𝙪𝙣𝙜 𝘽𝙪𝙡𝙡𝙤𝙫𝙚"
         )
