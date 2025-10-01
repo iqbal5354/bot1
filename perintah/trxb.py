daa83dc30a6fd36e986e0e0a08546ace765c907d
iqbal5354 <80057549+iqbal5354@users.noreply.github.com>
2025-10-02 04:58:15 +0700

    Update trxb.py

--- perintah/trxb.py
+++ perintah/trxb.py
@@ -4,9 +4,9 @@ def init(client):
     @client.on(events.NewMessage(pattern=r"^\.trxb$"))
     async def trx_menu(event):
         buttons = [
-            [Button.text("📋 Isi", resize=True)],
-            [Button.text("💰 Dana Masuk", resize=True)],
-            [Button.text("📝 Format", resize=True)],
-            [Button.text("📜 Aturan", resize=True)],
+            [Button.text("📋 Isi")],
+            [Button.text("💰 Dana Masuk")],
+            [Button.text("📝 Format")],
+            [Button.text("📜 Aturan")],
         ]
-        await event.respond("📖 **Menu TRX**\nPilih salah satu tombol:", buttons=buttons)
+        await event.reply("📖 **Menu TRX**\nPilih salah satu tombol:", buttons=buttons)
