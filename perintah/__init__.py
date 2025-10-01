0db77cb570a7683a5a0715163a48fdc3a0040088
iqbal5354 <80057549+iqbal5354@users.noreply.github.com>
2025-10-02 04:03:16 +0700

    Update __init__.py

--- perintah/__init__.py
+++ perintah/__init__.py
@@ -1,16 +1,12 @@
 from .commands import register_commands, init_owner
 from .trx import register_trx
-from .ai import register_ai, init_owner_ai
-from .speedtest import register_speedtest, init_owner_speed
 
 
 async def init_all_owner(client):
     """
-    Inisialisasi semua OWNER_ID untuk tiap modul.
+    Inisialisasi OWNER_ID (cukup sekali dari commands.py).
     """
     await init_owner(client)
-    await init_owner_ai(client)
-    await init_owner_speed(client)
 
 
 def init(client):
@@ -19,5 +15,3 @@ def init(client):
     """
     register_commands(client)
     register_trx(client)
-    register_ai(client)
-    register_speedtest(client)
